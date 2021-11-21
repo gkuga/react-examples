a4bd79907d2e6a6c404742668cd470ad64d6bc30
Gaku Yasui <yasui_gaku@cyberagent.co.jp>
2021-11-21 21:16:55 +0900

    Add material ui sample

---
+++ material-ui/README.md
@@ -0,0 +1,6 @@
+This sampele Refers to [React + Material-UIで管理画面を作成してみた](https://dev.classmethod.jp/articles/react-material-ui/).
+
+```
+npx create-react-app material-ui --template typescript
+
+```
---
+++ material-ui/src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import ProductPage from "./components/pages/ProductPage";
+import HomePage from "./components/pages/HomePage";
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
