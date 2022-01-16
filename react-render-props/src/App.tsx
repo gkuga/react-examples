a1cc140656d1fd99da21e116c7eda89ed2ddc069
Gaku Yasui <yasui_gaku@cyberagent.co.jp>
2022-01-16 23:13:30 +0900

    higher order component

--- react-render-props/src/App.tsx
+++ react-render-props/src/App.tsx
@@ -2,66 +2,44 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const Euro: React.FC<{amount: number}> = ({amount}) => <p>Euro: {amount * 0.86}</p>
-const Pound: React.FC<{amount: number}> = ({amount}) => <p>Pound: {amount * 0.76}</p>
-
-
 type OnIncrement = () => void
 type OnDecrement = () => void
 
-type AmountProps = {
-  renderAmountOne: (amount: number) => React.ReactElement;
-  renderAmountTwo: (amount: number) => React.ReactElement;
-}
+type CurrencyComponentType = React.FC<{amount: number}>
 
-const Amount: React.FC<AmountProps> = ({renderAmountOne, renderAmountTwo}) => {
-	const [amount, setAmount] = useState<number>(0)
+const withAmount = (currencyComponents: Array<CurrencyComponentType>) => 
+	() => {
+		const [amount, setAmount] = useState<number>(0)
 
-  const onIncrement = () => {
-    setAmount(amount + 1 )
-  }
+		const onIncrement = () => {
+			setAmount(amount + 1 )
+		}
 
-  const onDecrement = () => {
-    setAmount(amount - 1 )
-  }
+		const onDecrement = () => {
+			setAmount(amount - 1 )
+		}
 
-	return (
-		<div>
-			<span>US Dollar: {amount} </span>
+		return (
+			<div>
+				<span>US Dollar: {amount} </span>
 
-      {renderAmountTwo(amount)}
+				<button type="button" onClick={onIncrement}>
+					+
+				</button>
+				<button type="button" onClick={onDecrement}>
+					-
+				</button>
 
-			<button type="button" onClick={onIncrement}>
-				+
-			</button>
-			<button type="button" onClick={onDecrement}>
-				-
-			</button>
+				{currencyComponents.map((CurrencyComponent: CurrencyComponentType) => (
+					<CurrencyComponent amount={amount} />
+				))}
+			</div>
+		)
+	}
 
-      {renderAmountOne(amount)}
-		</div>
-	)
-}
-
-const App: React.FC = () => {
-	return (
-		<Amount
-			renderAmountOne={amount => (
-				<div>
-					<h2>My one Amount</h2>
-					<Pound amount={amount} />
-					<Euro amount={amount} />
-				</div>
-			)}
-			renderAmountTwo={amount => (
-				<div>
-					<h2>My other Amount</h2>
-					<Pound amount={amount} />
-					<Euro amount={amount} />
-				</div>
-			)}
-		/>
-	)
-}
+const Euro: CurrencyComponentType = ({amount}) => <p>Euro: {amount * 0.86}</p>
+const Pound: CurrencyComponentType = ({amount}) => <p>Pound: {amount * 0.76}</p>
+const CurrenciesWithAmount = withAmount([Euro, Pound])
+const App = () => <CurrenciesWithAmount />
 
 export default App
